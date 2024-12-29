6da31643f2385ef9e4bc76f1135ffd0e5ce8885a
Ta Chi Thanh Danh <22120049@student.hcmus.edu.vn>
2024-12-29 17:37:27 +0700

    Remove MistakeResponses.ts

--- src/interfaces/responses/MistakeResponse.ts
+++ src/interfaces/responses/MistakeResponse.ts
@@ -1,13 +1,12 @@
 // src/interfaces/responses/MistakeResponse.ts
-
 export interface MistakeResponse {
-    id: number;                 // ID của lỗi
-    user_id: number;           // ID của người dùng
-    lesson_id: number;         // ID của bài học
-    question_id: number;       // ID của câu hỏi
-    your_answer: string;       // Câu trả lời của người dùng
-    correct_answer: string;    // Câu trả lời đúng
-    active: boolean;           // Trạng thái hoạt động
-    created_at: string;        // Thời gian tạo
-    updated_at: string;        // Thời gian cập nhật
+  id: number; // ID của lỗi
+  userId: number; // ID của người dùng
+  lessonId: number; // ID của bài học
+  questionId: number; // ID của câu hỏi
+  yourAnswer: string; // Câu trả lời của người dùng
+  correctAnswer: string; // Câu trả lời đúng
+  active: boolean; // Trạng thái hoạt động
+  createdAt: string; // Thời gian tạo
+  updatedAt: string; // Thời gian cập nhật
 }
